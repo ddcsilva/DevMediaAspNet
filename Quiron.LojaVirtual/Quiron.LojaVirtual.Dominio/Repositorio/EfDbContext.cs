d35302432b1e99fa1ce94f78073875ae24c1ed72
Danilo Silva <danilo.silva@msn.com>
2021-02-09 21:58:52 -0300

    Criação da classe de context e configuração da string de conexão com o BD

---
+++ Quiron.LojaVirtual/Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using System.Data.Entity;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
